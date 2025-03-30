7f140d772505c9d17c55b83d0904fb5f0aea7797
unclemusclez <unclemusclez@gmail.com>
2025-03-30 00:50:52 -0400

    Changes Fixed Syntax: Corrected $global b_settings to $global_settings on line 30 (and ensured no other typos).

--- includes/admin.php
+++ includes/admin.php
@@ -27,7 +27,7 @@ function pmpro_matomo_settings_page() {
     } elseif ( $has_connect_matomo ) {
         $global_settings = get_option( 'wp_piwik_global_settings', [] );
         $site_settings = get_option( 'wp_piwik_settings', [] );
-        $site_id = isset( $site_settings['site_id'] ) ? $site_settings['site_id'] : (isset( $global_settings['default_site'] ) ? $global b_settings['default_site'] : '');
+        $site_id = isset( $site_settings['site_id'] ) ? $site_settings['site_id'] : (isset( $global_settings['default_site'] ) ? $global_settings['default_site'] : '');
         $tracker_url = isset( $site_settings['piwik_path'] ) ? $site_settings['piwik_path'] : (isset( $global_settings['piwik_path'] ) ? $global_settings['piwik_path'] : '');
     } elseif ( $has_matomo_analytics ) {
         $settings = new \WpMatomo\Settings();
